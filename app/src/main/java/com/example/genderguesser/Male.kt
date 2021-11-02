1aa1407f94aa1550446e6846b5b6bebed2092640
SbongiseniMbaraga <Sbongiseni2@hotmail.com>
2021-11-02 20:53:22 +0200

    added new layout

---
+++ app/src/main/java/com/example/genderguesser/Male.kt
@@ -0,0 +1,11 @@
+package com.example.genderguesser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Male : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_male)
+    }
+}
