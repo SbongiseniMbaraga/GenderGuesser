09f887628fc1c3cc36b5ab4e82a40382433a968b
SbongiseniMbaraga <Sbongiseni2@hotmail.com>
2021-11-07 13:52:25 +0200

    added

--- app/src/main/java/com/example/genderguesser/MainActivity.kt
+++ app/src/main/java/com/example/genderguesser/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.genderguesser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText = findViewById<EditText>(R.id.editText_name)
+
+        val url = "https://api.genderize.io/?name=peter"
+
+        editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP){
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 }
